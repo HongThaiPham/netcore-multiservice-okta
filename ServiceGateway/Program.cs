07a947d13fe03d6d390f7195b684cfa438f23ce8
Leo Pham <hongthaipro@gmail.com>
2022-03-19 14:42:05 +0700

    Done without docker

--- ServiceGateway/Program.cs
+++ ServiceGateway/Program.cs
@@ -1,3 +1,4 @@
+using Okta.AspNetCore;
 using ServiceGateway;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,13 +7,45 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 
+// Configure authentication middleware
+AddAuthMiddleware(builder);
+
 var app = builder.Build();
 
 // Expose the gateway via port "4300"
 app.Urls.Clear();
 app.Urls.Add("http://localhost:4300");
 
+// Federated Logging
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request for \"{context.Request.Path}\"");
+    await next();
+});
+
+// Federated Rate-Limiting
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Rate-Limit-Limit", "infinite");
+    await next();
+});
+
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Using Okta to generate tokens by forwarding the request.
+// To your clients, if you strip out Okta specific response headers,
+// they would have no idea that Okta is being used as the identity
+// provider!
+app.MapPost("/token", async context =>
+{
+    var client = app.Services.GetService<IHttpClientFactory>().CreateClient();
+
+    var message = context.CreateProxyHttpRequest(new Uri($"{builder.Configuration["Okta:Domain"]}/oauth2/default/v1/token"));
+    var response = await client.SendAsync(message);
+    await context.CopyProxyHttpResponse(response);
+});
 
 // We're routing clients to internal microservices, which many times
 // will have different public and internal URLs. Gateways can help to
@@ -33,6 +66,25 @@ app.MapGet("/sales/sku/{sku:required}", async context =>
     var message = context.CreateProxyHttpRequest(new Uri($"http://localhost:4302/sku/{sku}"));
     var response = await client.SendAsync(message);
     await context.CopyProxyHttpResponse(response);
-});
+}).RequireAuthorization();
 
 app.Run();
+
+
+void AddAuthMiddleware(WebApplicationBuilder builder)
+{
+    // This method is all it takes to use Okta to automatically test incoming
+    // bearer tokens for authentication!
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
+        options.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
+        options.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
+    })
+    .AddOktaWebApi(new OktaWebApiOptions()
+    {
+        OktaDomain = builder.Configuration["Okta:Domain"],
+    });
+
+    builder.Services.AddAuthorization();
+}
