60da1023e8f88804b83597760ea63de240278635
Leo Pham <hongthaipro@gmail.com>
2022-03-19 14:20:15 +0700

    Done service proxy

--- Service2/Program.cs
+++ Service2/Program.cs
@@ -1,23 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.Urls.Clear();
+app.Urls.Add("http://localhost:4302");
 
-app.UseAuthorization();
+app.UseRouting();
 
-app.MapControllers();
+app.MapGet("/sku/{sku:required}", (string sku) => $"You're looking at SKU {sku}!");
 
 app.Run();
--- ServiceGateway/Program.cs
+++ ServiceGateway/Program.cs
@@ -1,23 +1,38 @@
+using ServiceGateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Expose the gateway via port "4300"
+app.Urls.Clear();
+app.Urls.Add("http://localhost:4300");
+
+app.UseRouting();
+
+// We're routing clients to internal microservices, which many times
+// will have different public and internal URLs. Gateways can help to
+// keep routing centralized as a cross-cutting and customer-focused concern.
+app.MapGet("/warehouse", async context =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var client = app.Services.GetService<IHttpClientFactory>().CreateClient();
 
-app.UseAuthorization();
+    var message = context.CreateProxyHttpRequest(new Uri("http://localhost:4301/inventory"));
+    var response = await client.SendAsync(message);
+    await context.CopyProxyHttpResponse(response);
+});
 
-app.MapControllers();
+app.MapGet("/sales/sku/{sku:required}", async context =>
+{
+    var sku = context.Request.RouteValues["sku"];
+    var client = app.Services.GetService<IHttpClientFactory>().CreateClient();
+    var message = context.CreateProxyHttpRequest(new Uri($"http://localhost:4302/sku/{sku}"));
+    var response = await client.SendAsync(message);
+    await context.CopyProxyHttpResponse(response);
+});
 
 app.Run();
